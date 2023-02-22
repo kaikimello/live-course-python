e3d21389d4f5944f22c1b274a5c1bdb5dce006c2
Kaiki Mello dos Santos <kaiki.mello@gmail.com>
2023-02-22 16:48:19 -0300

    Adicionando .gitignore e aplicando filtros em logs

--- log/log_lesson.py
+++ log/log_lesson.py
@@ -1,21 +1,37 @@
 from logging import (CRITICAL, DEBUG, ERROR, INFO, WARNING, FileHandler,
-                     StreamHandler, basicConfig, critical, debug, error, info,
-                     warning)
+                     Filter, StreamHandler, basicConfig, critical, debug,
+                     error, info, warning)
+
+
+# Criando filtros para os logs
+class Filtro(Filter):
+    def filter(self, record):
+        if 'cartão' in record.msg:
+            record.msg = 'Vazou a senha do cartão!!! ATENÇÃO'
+            return True
+        return False 
+
+file_handler = FileHandler('logs.log', 'a')
+# Setando Warning no file_handler para que sejam salvos 
+# logs a partir do Warning 
+file_handler.setLevel(WARNING)
+# Adicionndo o filtro em file_handler 
+file_handler.addFilter(Filtro())
+
+
+# Da para criar níveis de gravidade para os handlers
+stream_handler = StreamHandler()
+
 
 # Forma como o log vai se comportar na aplicação
 basicConfig(
     level=DEBUG,
     format='%(levelname)s:%(asctime)s:%(message)s',
-    handlers=[StreamHandler(), FileHandler('logs.log', 'a')]
+    #Para cada handler especifico vai ter um level diferente
+    handlers=[file_handler, stream_handler]
 )
 
 # debug('Estou testando')
 # critical('Senha do usuário foi exposta')
 
-try:
-    x = int(input('Digite um numero'))
-
-    y = 10/x
-
-except ZeroDivisionError:
-    warning('Tentou dividir por zero')
+critical('cartão 12345')
